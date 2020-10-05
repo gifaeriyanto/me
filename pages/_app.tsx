07c5ac3f836549e6717f5d848a40b2e155e1a43d
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-10-06 05:39:38 +0800

    register next offline runtime

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/core';
 import { theme, DarkThemeContext } from '@utils/theme';
+import { register, unregister } from 'next-offline/runtime';
 import { AppProps } from 'next/app';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    register();
+    return () => {
+      unregister();
+    };
+  }, []);
 
   return (
     <DarkThemeContext.Provider value={{ darkMode, setDarkMode }}>
