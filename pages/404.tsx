30c9034119d66165bb9cf85579b061d4e43fc0bb
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-10-11 19:44:50 +0800

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,25 @@
+import { Box, Flex, Heading, Text } from '@chakra-ui/core';
+import { Typing } from '@components/typing';
 import { NextPage } from 'next';
-import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 import React from 'react';
 
 const Index: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>404</title>
-      </Head>
+      <NextSeo title="404" description="Page not found" />
 
-      <div>404</div>
+      <Flex justify="center" align="center" direction="column" flex="1">
+        <Heading
+          as="h1"
+          fontSize="xl"
+          color="highlight"
+          gridColumn={[1, null, 2]}
+        >
+          <Typing text="404" id="404-title-page" />
+        </Heading>
+        <Text>Page not found</Text>
+      </Flex>
     </>
   );
 };
