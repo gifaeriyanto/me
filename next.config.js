40b5b9cec8accee146c0f58b7462d022b62618a2
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-11-21 01:14:43 +0800

    update env

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    API_GOOGLE_ANALYTIC_KEY: process.env.API_GOOGLE_ANALYTIC_KEY,
   },
 
   // webpack config
