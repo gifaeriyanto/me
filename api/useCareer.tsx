4f5d206ed4ad5007155118063ecc1d85b4119316
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-29 02:55:28 +0800

    fix typecheck

--- api/useCareer.tsx
+++ api/useCareer.tsx
@@ -10,8 +10,8 @@ export interface ICareerData {
   company: string;
   company_website: string;
   location: string;
-  start_date: Date;
-  end_date: Date;
+  start_date: any;
+  end_date: any;
 }
 
 export interface IFetchCareerResult {
